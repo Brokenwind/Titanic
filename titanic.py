a0569dabbda361060a2d6bbc1d8f348ce08b4742
Brokenwind <wangkun6536@gmail.com>
2018-11-08 19:12:14 -0500

    Added methods to add missing data

--- titanic.py
+++ titanic.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor
 
 def load_data():
     train_data = pd.read_csv('data/train.csv')
@@ -18,6 +19,19 @@ def add_miss_data(train_data):
     train_data['Embarked'][train_data['Embarked'].isnull()] = embarked_mode
     # 可以赋一个代表缺失的值，比如‘U0’。因为缺失本身也可能代表着一些隐含信息
     train_data['Cabin'] = train_data['Cabin'].fillna('U0')
+    # 使用回归 随机森林等模型来预测缺失属性的值。因为Age在该数据集里是一个相当重要的特征
+    age_df = train_data[['Age','Survived','Fare', 'Parch', 'SibSp', 'Pclass']]
+    age_df_notnull = age_df.loc[(train_data['Age'].notnull())]
+    age_df_isnull = age_df.loc[(train_data['Age'].isnull())]
+    X = age_df_notnull.values[:,1:]
+    Y = age_df_notnull.values[:,0]
+    # use RandomForestRegression to train data
+    RFR = RandomForestRegressor(n_estimators=1000, n_jobs=-1)
+    RFR.fit(X,Y)
+    predictAges = RFR.predict(age_df_isnull.values[:,1:])
+    train_data.loc[train_data['Age'].isnull(), ['Age']]= predictAges
+    
+    return train_data
 
 if __name__ == '__main__':
     train_data, test_data = load_data()
