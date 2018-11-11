3822a672ecaf99ca8c58d0864f0f4eea9a977a78
Brokenwind <wangkun6536@163.com>
2018-11-11 23:47:38 +0800

    Added processing of age and ticket

--- titanic.py
+++ titanic.py
@@ -1,8 +1,13 @@
+import sys
 import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import ensemble
+from sklearn import model_selection
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 
 def load_data():
@@ -13,6 +18,7 @@ def load_data():
     test_data.info()
     return train_data, test_data
 
+
 def add_miss_data(train_data):
     embarked_mode = train_data['Embarked'].dropna().mode().values
     print(embarked_mode)
@@ -21,25 +27,27 @@ def add_miss_data(train_data):
     # 可以赋一个代表缺失的值，比如‘U0’。因为缺失本身也可能代表着一些隐含信息
     train_data['Cabin'] = train_data['Cabin'].fillna('U0')
     # 使用回归 随机森林等模型来预测缺失属性的值。因为Age在该数据集里是一个相当重要的特征
-    age_df = train_data[['Age','Survived','Fare', 'Parch', 'SibSp', 'Pclass']]
+    age_df = train_data[['Age', 'Survived', 'Fare', 'Parch', 'SibSp', 'Pclass']]
     age_df_notnull = age_df.loc[(train_data['Age'].notnull())]
     age_df_isnull = age_df.loc[(train_data['Age'].isnull())]
-    X = age_df_notnull.values[:,1:]
-    Y = age_df_notnull.values[:,0]
+    X = age_df_notnull.values[:, 1:]
+    Y = age_df_notnull.values[:, 0]
     # use RandomForestRegression to train data
     RFR = RandomForestRegressor(n_estimators=1000, n_jobs=-1)
-    RFR.fit(X,Y)
-    predictAges = RFR.predict(age_df_isnull.values[:,1:])
-    train_data.loc[train_data['Age'].isnull(), ['Age']]= predictAges
-    
+    RFR.fit(X, Y)
+    predictAges = RFR.predict(age_df_isnull.values[:, 1:])
+    train_data.loc[train_data['Age'].isnull(), ['Age']] = predictAges
+
     return train_data
+
 
 def get_combined_data():
     train_df_org = pd.read_csv('data/train.csv')
     test_df_org = pd.read_csv('data/test.csv')
-    test_df_org['Survived']=0
+    test_df_org['Survived'] = 0
     combined_train_test = train_df_org.append(test_df_org)
     return combined_train_test
+
 
 def process_embarked(combined_train_test):
     '''
@@ -47,10 +55,12 @@ def process_embarked(combined_train_test):
     combined_train_test['Embarked'].fillna(combined_train_test['Embarked'].mode().iloc[0], inplace=True)
     combined_train_test['Embarked'] = pd.factorize(combined_train_test['Embarked'])[0]
     # df['column'] is a Series, but df[['column']] is a DataFrame
-    emb_dummies_df = pd.get_dummies(combined_train_test['Embarked'], prefix=combined_train_test[['Embarked']].columns[0])
-    combined_train_test = pd.concat([combined_train_test, emb_dummies_df],axis=1)
+    emb_dummies_df = pd.get_dummies(combined_train_test['Embarked'],
+                                    prefix=combined_train_test[['Embarked']].columns[0])
+    combined_train_test = pd.concat([combined_train_test, emb_dummies_df], axis=1)
 
     return combined_train_test
+
 
 def process_sex(combined_train_test):
     '''
@@ -58,42 +68,125 @@ def process_sex(combined_train_test):
     combined_train_test['Sex'] = pd.factorize(combined_train_test['Sex'])[0]
     # df['column'] is a Series, but df[['column']] is a DataFrame
     sex_dummies_df = pd.get_dummies(combined_train_test['Sex'], prefix=combined_train_test[['Sex']].columns[0])
-    combined_train_test = pd.concat([combined_train_test, sex_dummies_df],axis=1)
+    combined_train_test = pd.concat([combined_train_test, sex_dummies_df], axis=1)
 
     return combined_train_test
+
 
 def process_name(combined_train_test):
     combined_train_test['Title'] = combined_train_test['Name'].map(lambda x: re.compile(", (.*)\.").findall(x)[0])
     # map similar titile to one specified
     title_dict = {}
     title_dict.update(dict.fromkeys(['Capt', 'Col', 'Major', 'Dr', 'Rev'], 'Officer'))
-    title_dict.update(dict.fromkeys(['Don', 'Sir', 'the Countess', 'Dona', 'Lady'], 'Royalty')) 
-    title_dict.update(dict.fromkeys(['Mme', 'Ms', 'Mrs'], 'Mrs')) 
-    title_dict.update(dict.fromkeys(['Mlle', 'Miss'], 'Miss')) 
-    title_dict.update(dict.fromkeys(['Mr'], 'Mr')) 
-    title_dict.update(dict.fromkeys(['Master','Jonkheer'], 'Master'))
+    title_dict.update(dict.fromkeys(['Don', 'Sir', 'the Countess', 'Dona', 'Lady'], 'Royalty'))
+    title_dict.update(dict.fromkeys(['Mme', 'Ms', 'Mrs'], 'Mrs'))
+    title_dict.update(dict.fromkeys(['Mlle', 'Miss'], 'Miss'))
+    title_dict.update(dict.fromkeys(['Mr'], 'Mr'))
+    title_dict.update(dict.fromkeys(['Master', 'Jonkheer'], 'Master'))
     combined_train_test['Title'] = combined_train_test['Title'].map(title_dict)
     combined_train_test['Title'] = pd.factorize(combined_train_test['Title'])[0]
-    title_dummies_df = pd.get_dummies(combined_train_test['Title'], prefix=combined_train_test[['Title']].columns[0])    
-    combined_train_test = pd.concat([combined_train_test,title_dummies_df], axis=1)
+    title_dummies_df = pd.get_dummies(combined_train_test['Title'], prefix=combined_train_test[['Title']].columns[0])
+    combined_train_test = pd.concat([combined_train_test, title_dummies_df], axis=1)
     combined_train_test['Name_len'] = combined_train_test['Name'].map(len)
 
     return combined_train_test
 
+
 def process_fare(combined_train_test):
-    combined_train_test['Fare'] = combined_train_test[['Fare']].fillna(combined_train_test.groupby('Pclass').transform(np.mean))
-    combined_train_test['Group_Ticket'] = combined_train_test['Fare'].groupby(combined_train_test['Ticket']).transform('count')
+    combined_train_test['Fare'] = combined_train_test[['Fare']].fillna(
+        combined_train_test.groupby('Pclass').transform(np.mean))
+    combined_train_test['Group_Ticket'] = combined_train_test['Fare'].groupby(combined_train_test['Ticket']).transform(
+        'count')
     combined_train_test['Fare'] = combined_train_test['Fare'] / combined_train_test['Group_Ticket']
     combined_train_test.drop(['Group_Ticket'], axis=1, inplace=True)
-    combined_train_test['Fare_bin_id'] = pd.factorize(combined_train_test['Fare_bin'])
+    # 票价划分等级
+    combined_train_test['Fare_bin'] = pd.qcut(combined_train_test['Fare'], 5)
+    combined_train_test['Fare_bin_id'] = pd.factorize(combined_train_test['Fare_bin'])[0]
+    fare_bin_dummies_df = pd.get_dummies(combined_train_test['Fare_bin_id']).rename(columns=lambda x: 'Fare_' + str(x))
+    combined_train_test = pd.concat([combined_train_test, fare_bin_dummies_df], axis=1)
+    combined_train_test.drop(['Fare_bin'], axis=1, inplace=True)
+    return combined_train_test
+
+
+def family_size_category(family_size):
+    if family_size <= 1:
+        return 'Single'
+    elif family_size <= 4:
+        return 'Small'
+    else:
+        return 'Large'
+
+
+def process_family_size(combined_train_test):
+    from sklearn.preprocessing import LabelEncoder
+
+    combined_train_test['Family_Size'] = combined_train_test['Parch'] + combined_train_test['SibSp'] + 1
+    combined_train_test['Family_Cate'] = combined_train_test['Family_Size'].map(family_size_category)
+    le_family = LabelEncoder()
+    le_family.fit(np.array(['Single', 'Small', 'Large']))
+    combined_train_test['Family_Cate'] = le_family.transform(combined_train_test['Family_Cate'])
+    family_size_dummies_df = pd.get_dummies(combined_train_test['Family_Cate'],
+                                            prefix=combined_train_test[['Family_Cate']].columns[0])
+    combined_train_test = pd.concat([combined_train_test, family_size_dummies_df], axis=1)
+    
+    return combined_train_test
+
+
+def process_age(combined_train_test):
+    missing_age_df = pd.DataFrame(combined_train_test[['Age', 'Embarked', 'Sex', 'Title', 'Name_len', 'Family_Size',
+                                                       'Family_Cate', 'Fare', 'Fare_bin_id', 'Pclass']])
+    missing_age_train = missing_age_df[missing_age_df['Age'].notnull()]
+    missing_age_test = missing_age_df[missing_age_df['Age'].isnull()]
+    missing_age_train_X = missing_age_train.drop(['Age'], axis=1)
+    missing_age_train_Y = missing_age_train['Age']
+    missing_age_test_X = missing_age_test.drop(['Age'], axis=1)
+    # model gbm
+    gbm_reg = GradientBoostingRegressor(random_state=42)
+    gbm_reg_param_grid = {'n_estimators': [2000], 'max_depth': [4], 'learning_rate': [0.01], 'max_features': [3]}
+    gbm_reg_grid = model_selection.GridSearchCV(gbm_reg,gbm_reg_param_grid,cv=10, n_jobs=25,verbose=1, scoring='neg_mean_squared_error')
+    gbm_reg_grid.fit(missing_age_train_X,missing_age_train_Y)
+    print('Age feature Best GB Params:' + str(gbm_reg_grid.best_params_))
+    print('Age feature Best GB Score:' + str(gbm_reg_grid.best_score_))
+    print('GB Train error for age:'+str(gbm_reg_grid.score(missing_age_train_X,missing_age_train_Y)))
+    missing_age_test.loc[:,'Age_GB'] = gbm_reg_grid.predict(missing_age_test_X)
+    print(missing_age_test['Age_GB'][:4])
+    # model rf
+    rf_reg = RandomForestRegressor()
+    rf_reg_param_grid = {'n_estimators': [200], 'max_depth': [5], 'random_state': [0]}
+    rf_reg_grid = model_selection.GridSearchCV(rf_reg,rf_reg_param_grid,cv=10, n_jobs=25,verbose=1, scoring='neg_mean_squared_error')
+    rf_reg_grid.fit(missing_age_train_X,missing_age_train_Y)
+    print('Age feature Best RF Params:' + str(rf_reg_grid.best_params_))
+    print('Age feature Best RF Score:' + str(rf_reg_grid.best_score_))
+    print('GB Train error for age:'+str(rf_reg_grid.score(missing_age_train_X,missing_age_train_Y)))
+    missing_age_test.loc[:,'Age_RF'] = rf_reg_grid.predict(missing_age_test_X)
+    print(missing_age_test['Age_RF'][:4])
+
+    missing_age_test.loc[:,'Age'] = missing_age_test[['Age_GB','Age_RF']].mean(axis=1)
+    print(missing_age_test.loc[:,'Age'])
+    combined_train_test.loc[combined_train_test['Age'].isnull(), 'Age'] = missing_age_test['Age']
+
+    return combined_train_test
+
+def process_ticket(combined_train_test):
+    combined_train_test['Ticket_Letter'] = combined_train_test['Ticket'].str.split().str[0]
+    combined_train_test['Ticket_Letter'] = combined_train_test['Ticket_Letter'].apply(lambda x: 'U0' if x.isnumeric() else x)
+    # 如果要提取数字信息，则也可以这样做，现在我们对数字票单纯地分为一类。
+    # combined_train_test['Ticket_Number'] = combined_train_test['Ticket'].apply(lambda x: pd.to_numeric(x, errors='coerce'))
+    # combined_train_test['Ticket_Number'].fillna(0, inplace=True)
+    # 将 Ticket_Letter factorize
+    combined_train_test['Ticket_Letter'] = pd.factorize(combined_train_test['Ticket_Letter'])[0]
+
     return combined_train_test
 
 if __name__ == '__main__':
-    #train_data, test_data = load_data()
-    #add_miss_data(train_data)
+    # train_data, test_data = load_data()
+    # add_miss_data(train_data)
     combined_train_test = get_combined_data()
-    #combined_train_test = process_embarked(combined_train_test)
-    #combined_train_test = process_sex(combined_train_test)
-    #combined_train_test = process_name(combined_train_test)
+    combined_train_test = process_embarked(combined_train_test)
+    combined_train_test = process_sex(combined_train_test)
+    combined_train_test = process_name(combined_train_test)
     combined_train_test = process_fare(combined_train_test)
+    combined_train_test = process_family_size(combined_train_test)
+    combined_train_test = process_age(combined_train_test)
+    combined_train_test = process_ticket(combined_train_test)
     print(combined_train_test.info())
