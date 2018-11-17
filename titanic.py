e12ee59a41ac928fc046cb36d4e5b3d031dd742a
Brokenwind <wangkun6536@163.com>
2018-11-17 03:13:17 -0500

    add: use stacking methods to predict result

--- titanic.py
+++ titanic.py
@@ -16,6 +16,10 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
 
 def load_data():
     train_data = pd.read_csv('data/train.csv')
@@ -253,6 +257,64 @@ def get_top_n_features(titanic_train_data_X, titanic_train_data_Y, top_n_feature
 
     return features_top_n, features_importance
 
+def get_out_fold(clf, x_train, y_train, x_test):
+    n_train = titanic_train_data_X.shape[0]
+    n_test = titanic_test_data_X.shape[0]
+    SEED = 0
+    NFOLDS = 7
+    kf = KFold(n_splits=NFOLDS, random_state=SEED, shuffle=False)
+
+    oof_train = np.zeros((n_train,))
+    oof_test = np.zeros((n_test,))
+    oof_test_skf = np.empty((NFOLDS, n_test))
+
+    for i, (train_index, test_index) in enumerate(kf.split(x_train)):
+        x_tr = x_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = x_train[test_index]
+        clf.fit(x_tr, y_tr)
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(x_test)
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+def stacking_level_one(x_train, y_train, x_test):
+    rf = RandomForestClassifier(n_estimators=500, warm_start=True, max_features='sqrt', max_depth=6,
+                                min_samples_split=3, min_samples_leaf=2, n_jobs=-1, verbose=0)
+    ada = AdaBoostClassifier(n_estimators=500, learning_rate=0.1)
+    et = ExtraTreesClassifier(n_estimators=500, n_jobs=-1, max_depth=8, min_samples_leaf=2, verbose=0)
+    gb = GradientBoostingClassifier(n_estimators=500, learning_rate=0.008, min_samples_split=3, min_samples_leaf=2,
+                                    max_depth=5, verbose=0)
+    dt = DecisionTreeClassifier(max_depth=8)
+    knn = KNeighborsClassifier(n_neighbors=2)
+    svm = SVC(kernel='linear', C=0.025)
+
+    # Create our OOF train and test predictions. These base results will be used as new features
+    rf_oof_train, rf_oof_test = get_out_fold(rf, x_train, y_train, x_test)
+    # Random Forest
+    ada_oof_train, ada_oof_test = get_out_fold(ada, x_train, y_train, x_test)
+    # AdaBoost
+    et_oof_train, et_oof_test = get_out_fold(et, x_train, y_train, x_test)
+    # Extra Trees
+    gb_oof_train, gb_oof_test = get_out_fold(gb, x_train, y_train, x_test)
+    # Gradient Boost
+    dt_oof_train, dt_oof_test = get_out_fold(dt, x_train, y_train, x_test)
+    # Decision Tree
+    knn_oof_train, knn_oof_test = get_out_fold(knn, x_train, y_train, x_test)
+    # KNeighbors
+    svm_oof_train, svm_oof_test = get_out_fold(svm, x_train, y_train, x_test)
+
+    oof_train = (rf_oof_train, ada_oof_train, et_oof_train, gb_oof_train, dt_oof_train, knn_oof_train, svm_oof_train)
+    oof_test = (rf_oof_test, ada_oof_test, et_oof_test, gb_oof_test, dt_oof_test, knn_oof_test, svm_oof_test)
+
+    return oof_train, oof_test
+
+def stacking_level_two(x_train, y_train, x_test):
+    gbm = XGBClassifier(n_estimators=2000, max_depth=4, min_child_weight=2, gamma=0.9, subsample=0.8,colsample_bytree=0.8, objective= 'binary:logistic', nthread= -1, scale_pos_weight=1)
+    gbm.fit(x_train, y_train)
+    predictions = gbm.predict(x_test)
+    print(predictions)
+    return predictions
 
 if __name__ == '__main__':
     # train_data, test_data = load_data()
@@ -273,4 +335,17 @@ if __name__ == '__main__':
     titanic_train_data_Y = train_data['Survived'] 
     titanic_test_data_X = test_data.drop(['Survived'],axis=1)
     features_top_n, features_importance = get_top_n_features(titanic_train_data_X, titanic_train_data_Y, 10)
-    print(combined_train_test.info())
+    # just use the selected features
+    titanic_train_data_X = pd.DataFrame(titanic_train_data_X[features_top_n])
+    titanic_test_data_X = pd.DataFrame(titanic_test_data_X[features_top_n])
+    # Create Numpy arrays of train, test and target (Survived) dataframes to feed into our models
+    x_train = titanic_train_data_X.values
+    # Creates an array of the train data
+    y_train = titanic_train_data_Y.values
+    # Creats an array of the test data
+    x_test = titanic_test_data_X.values
+    oof_train, oof_test = stacking_level_one(x_train,y_train,x_test)
+    x_train_xg = np.concatenate(oof_train, axis=1)
+    x_test_xg = np.concatenate(oof_test, axis=1)
+    stacking_level_two(x_train_xg, y_train, x_test_xg)
+    
